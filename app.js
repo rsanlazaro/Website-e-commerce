2d35ffe05e890bc09aeb59947bb8b15010dbc0dd
rsanlazaro <rsanlazaro@hotmail.com>
2021-06-22 20:29:19 -0500

    [FIX] Port and Procfile

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 app.use(express.static('public'));
 
 
-app.listen(process.env.PORT || 3000, ()=>{
+app.listen(process.env.PORT || 3000, function(){
     console.log('Servidor corriendo en el puerto 3000');
 });
 
